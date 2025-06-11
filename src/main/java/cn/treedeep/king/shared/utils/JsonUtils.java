64a2bd5316f105afb3f7ff1be90c81295d2a5cc7
zhougm <shushen@trdeep.com>
2025-06-11 19:24:27 +0800

    优化JsonUtils

--- src/main/java/cn/treedeep/king/shared/utils/JsonUtils.java
+++ src/main/java/cn/treedeep/king/shared/utils/JsonUtils.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 提供基于Jackson库的JSON工具方法。
@@ -15,8 +16,7 @@ import java.util.Map;
 @Slf4j
 public class JsonUtils {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
+    private static final ObjectMapper objectMapper = SpringBeanUtil.getBean("defaultObjectMapper", ObjectMapper.class);
 
     /**
      * 将对象转换为JSON字符串。
@@ -29,24 +29,7 @@ public class JsonUtils {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
             log.error("Failed to convert object to JSON string", e);
-            return "{\"error\": \"Failed to convert object to JSON string\"}";
-        }
-    }
-
-    /**
-     * 将JSON字符串转换为指定类型的对象。
-     *
-     * @param <T>   目标对象的类型
-     * @param json  需要转换的JSON字符串
-     * @param clazz 目标对象的类
-     * @return 转换后的对象，如果转换失败则返回null
-     */
-    public static <T> T fromJson(String json, Class<T> clazz) {
-        try {
-            return objectMapper.readValue(json, clazz);
-        } catch (IOException e) {
-            log.error("Failed to convert JSON string to object", e);
-            return null;
+            return "{\"error\": \"" + e.getMessage() + "\"}";
         }
     }
 
@@ -77,7 +60,28 @@ public class JsonUtils {
             return objectMapper.writeValueAsString(map);
         } catch (JsonProcessingException e) {
             log.error("Failed to convert map to JSON string", e);
-            return "{\"error\": \"Failed to convert map to JSON string\"}";
+            return "{\"error\": \"" + e.getMessage() + "\"}";
+        }
+    }
+
+    /**
+     * 将JSON字符串转换为指定类型的对象。
+     *
+     * @param <T>   目标对象的类型
+     * @param json  需要转换的JSON字符串
+     * @param clazz 目标对象的类
+     * @return 转换后的对象，如果转换失败则返回null
+     */
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        return fromJsonOpt(json, clazz).orElse(null);
+    }
+
+    public static <T> Optional<T> fromJsonOpt(String json, Class<T> clazz) {
+        try {
+            return Optional.of(objectMapper.readValue(json, clazz));
+        } catch (IOException e) {
+            log.error("Failed to convert JSON string to object", e);
+            return Optional.empty();
         }
     }
 
@@ -90,11 +94,15 @@ public class JsonUtils {
      * @return 转换后的对象，如果转换失败则返回null
      */
     public static <T> T fromJson(String json, TypeReference<T> typeReference) {
+        return fromJsonOpt(json, typeReference).orElse(null);
+    }
+
+    public static <T> Optional<T> fromJsonOpt(String json, TypeReference<T> typeReference) {
         try {
-            return objectMapper.readValue(json, typeReference);
+            return Optional.of(objectMapper.readValue(json, typeReference));
         } catch (IOException e) {
             log.error("Failed to convert JSON string to object", e);
-            return null;
+            return Optional.empty();
         }
     }
 }
