a8e62884824e79081ce441d9fae9e3013b3a600a
zhougm <shushen@trdeep.com>
2025-06-11 16:15:54 +0800

    添加data字段

--- src/main/java/cn/treedeep/king/core/domain/DomainEvent.java
+++ src/main/java/cn/treedeep/king/core/domain/DomainEvent.java
@@ -17,13 +17,16 @@ import java.util.UUID;
 @Comment("领域事件表")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Data
-@org.jmolecules.event.annotation.DomainEvent
 public abstract class DomainEvent {
 
     @Id
     @Comment("事件ID")
     @Column(name = "event_id")
     private final String eventId;
+
+    @Comment("数据")
+    @Column(name = "data", columnDefinition = "TEXT")
+    private String data;
 
     @Comment("事件发生时间")
     @Column(name = "occurred_on")
@@ -36,6 +39,7 @@ public abstract class DomainEvent {
     @Comment("聚合根版本号")
     @Column(name = "aggregate_version")
     private Long aggregateVersion;
+
 
     protected DomainEvent() {
         this.eventId = UUID.randomUUID().toString();
